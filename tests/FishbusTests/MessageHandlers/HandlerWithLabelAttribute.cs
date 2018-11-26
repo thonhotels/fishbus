7de804bc207ea189a8171b18ea4d542be70d131c
Vegar Eilertsen <vegar.eilertsen@gmail.com>
2018-11-26 11:22:32 +0100

    Add support for using an attribute to connect message handlers to message labels (#1)

---
+++ tests/FishbusTests/MessageHandlers/HandlerWithLabelAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Thon.Hotels.FishBus;
+
+namespace FishbusTests.MessageHandlers
+{
+    [MessageLabel("A.Custom.Message.Label")]
+    public class MessageWithLabelAttribute
+    {
+
+    }
+
+    public class HandlerWithLabelAttribute : IHandleMessage<MessageWithLabelAttribute>
+    {
+        public Task Handle(MessageWithLabelAttribute message, Func<Task> markAsComplete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
