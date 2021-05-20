5fa2cc020c2e8f73ae6c616d79406dacf5f1004a
Vegar Eilertsen <vegar.eilertsen@gmail.com>
2021-05-20 14:57:48 +0200

    Add support for loading message handlers from multiple assemblies

--- src/ServicesExtensions.cs
+++ src/ServicesExtensions.cs
@@ -11,35 +11,53 @@ namespace Thon.Hotels.FishBus
         /// and register them in the .NET Core IoC.
         /// Fishbus framework classes are also registered
         /// <param name="assembly">Assembly that contains the MessageHandlers</param>
-        public static IServiceCollection ConfigureMessaging(this IServiceCollection services, Assembly assembly = null)
+        public static IServiceCollection ConfigureMessaging(this IServiceCollection services)
         {
             var logCorrelationHandler = new LogCorrelationHandler(false);
-            return services.ConfigureMessaging(logCorrelationHandler, assembly ?? Assembly.GetCallingAssembly());
+            return services.ConfigureMessaging(logCorrelationHandler, new[] {Assembly.GetCallingAssembly()});
         }
 
-        public static IServiceCollection ConfigureMessagingWithCorrelationLogging(this IServiceCollection services, Assembly assembly = null)
+        public static IServiceCollection ConfigureMessaging(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            var logCorrelationHandler = new LogCorrelationHandler(false);
+            return services.ConfigureMessaging(logCorrelationHandler, assemblies);
+        }
+
+        public static IServiceCollection ConfigureMessagingWithCorrelationLogging(this IServiceCollection services)
         {
             var logCorrelationHandler = new LogCorrelationHandler(true);
-            return services.ConfigureMessaging(logCorrelationHandler, assembly ?? Assembly.GetCallingAssembly());
+            return services.ConfigureMessaging(logCorrelationHandler, new[] {Assembly.GetCallingAssembly()});
         }
 
-        public static IServiceCollection ConfigureMessagingWithCorrelationLogging(this IServiceCollection services, LogCorrelationOptions logCorrelationOptions, Assembly assembly = null)
+        public static IServiceCollection ConfigureMessagingWithCorrelationLogging(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            var logCorrelationHandler = new LogCorrelationHandler(true);
+            return services.ConfigureMessaging(logCorrelationHandler, assemblies);
+        }
+
+        public static IServiceCollection ConfigureMessagingWithCorrelationLogging(this IServiceCollection services, LogCorrelationOptions logCorrelationOptions)
         {
             var logCorrelationHandler = new LogCorrelationHandler(true, logCorrelationOptions);
-            return services.ConfigureMessaging(logCorrelationHandler, assembly ?? Assembly.GetCallingAssembly());
+            return services.ConfigureMessaging(logCorrelationHandler, new[] {Assembly.GetCallingAssembly()});
         }
 
-        private static IServiceCollection ConfigureMessaging(this IServiceCollection services, LogCorrelationHandler logCorrelationHandler, Assembly assembly)
+        public static IServiceCollection ConfigureMessagingWithCorrelationLogging(this IServiceCollection services, LogCorrelationOptions logCorrelationOptions, params Assembly[] assemblies)
+        {
+            var logCorrelationHandler = new LogCorrelationHandler(true, logCorrelationOptions);
+            return services.ConfigureMessaging(logCorrelationHandler, assemblies);
+        }
+
+        private static IServiceCollection ConfigureMessaging(this IServiceCollection services, LogCorrelationHandler logCorrelationHandler, Assembly[] assemblies)
         {
             if (logCorrelationHandler == null)
                 throw new ArgumentNullException(nameof(logCorrelationHandler));
             MessageHandlerTypes
-                .GetAll(assembly)
+                .GetAll(assemblies)
                 .ToList()
                 .ForEach(t => services.AddTransient(t));
             services
                 .AddSingleton<MessagingConfiguration>()
-                .AddSingleton(p => new MessageHandlerRegistry(() => MessageHandlerTypes.GetAll(assembly)))
+                .AddSingleton(p => new MessageHandlerRegistry(() => MessageHandlerTypes.GetAll(assemblies)))
                 .AddSingleton(logCorrelationHandler)
                 .AddHostedService<MessagingService>();
 
